85c238d4f868c43d7d9c59e5fb921aa8b7e802db
Hafureluspatha <xxxddr3@gmail.com>
2015-10-09 23:13:55 +0300

    describe Song class

---
+++ Song.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Song
+{
+	public int id {get; set;}
+	public int owner_id { get; set; }
+	public string artist { get; set; }
+	public string title { get; set; }
+	public int duration { get; set; }
+	public string url { get; set; }
+	public int lyrics_id { get; set; }
+	public int genre_id { get; set; }
+}
