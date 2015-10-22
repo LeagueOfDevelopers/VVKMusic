40477511bb6c41351d3b71f91d6bdc2dd91dbdfc
Hafureluspatha <xxxddr3@gmail.com>
2015-10-22 15:15:34 +0300

    Rename "Download" module and all related stuff to Downloader

--- VVKMusic/Download/IDownloader.cs
+++ VVKMusic/Download/IDownloader.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Download
+namespace Downloader
 {
-    interface IDownload
+    interface IDownloader
     {
         // int DownloadSong(Song[] songMas);
     }
