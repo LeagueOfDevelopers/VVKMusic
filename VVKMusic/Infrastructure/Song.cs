bf76765e2ac28afceae64208c1f2f9a898686daa
Hafureluspatha <xxxddr3@gmail.com>
2015-10-23 15:42:55 +0300

    Перемещение класса Song в модуль Infrastructure

---
+++ VVKMusic/Infrastructure/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        public int OwnerID { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string url { get; set; }
+        public int LyricsID { get; set; }
+        public int GenreID { get; set; }
+    }
+}
