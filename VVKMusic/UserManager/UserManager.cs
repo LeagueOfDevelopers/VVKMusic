8e135721519a73846b13c6fe63c0e8852b307cdc
Hafureluspatha <xxxddr3@gmail.com>
2015-11-10 15:51:09 +0300

    Заглушка класса UserManager

---
+++ VVKMusic/UserManager/UserManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Status = Common.Common.Status;
+
+namespace UserManager
+{
+    class UserManager : IUserManager 
+    {
+        Status AddUser(User user)
+        {
+            return Status.OK;
+        }
+        Status UpdateUserSettings(string userID, Settings settings)
+        {
+            return Status.OK;
+        }
+        Status UpdateUserListOfSongs(Song[] SongMas, User user)
+        {
+            return Status.OK;
+        }
+        Status RemoveUser(string userID)
+        {
+            return Status.OK;
+        }
+        Song[] GetUserListOfSongs(string userID)
+        {
+            return null;
+        }
+        User[] GetListOfUsers()
+        {
+            return null;
+        }
+    }
+}
