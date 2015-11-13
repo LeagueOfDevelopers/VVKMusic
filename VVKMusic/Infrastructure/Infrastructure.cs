35f2f6370e2c35d98e4b5f423f5bf604d5281c91
Hafureluspatha <xxxddr3@gmail.com>
2015-11-13 11:48:37 +0300

    Реализация сериализации в Infrastructure. Код компилируется, но не тестировался.

--- VVKMusic/Infrastructure/Infrastructure.cs
+++ VVKMusic/Infrastructure/Infrastructure.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Status = Common.Common.Status;
+using Common;
+using System.IO;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 namespace Infrastructure
 {
@@ -13,13 +18,44 @@ namespace Infrastructure
         {
             return Status.OK;
         }
-        public Status SaveListOfUsers()
+        public Status SaveListOfUsers(List<User> listOfUsers)
         {
+            FileStream fs = new FileStream("Users.dat", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(fs, listOfUsers);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
             return Status.OK;
         }
-        public Status LoadListOfUsers()
+        public List<User> LoadListOfUsers()
         {
-            return Status.OK;
+            FileStream fs = new FileStream("Users.dat", FileMode.Open);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                return (List<User>)formatter.Deserialize(fs);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
         }
     }
 }
