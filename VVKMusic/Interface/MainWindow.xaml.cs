2908e8b51c73bf1621248f1e93ed508bd31ae9c7
Hafureluspatha <xxxddr3@gmail.com>
2016-02-05 09:47:17 +0800

    closes #27

--- VVKMusic/Interface/MainWindow.xaml.cs
+++ VVKMusic/Interface/MainWindow.xaml.cs
@@ -1,26 +1,13 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Common;
-using Downloader;
-using Infrastructure;
-using Player;
-using Playlist;
-using UserManager;
-using VKAPI;
-using Status = Common.Common.Status;
 
 namespace Interface
 {
