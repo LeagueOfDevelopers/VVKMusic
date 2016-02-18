1f749764441397a837703aaefc3cd8c3e55ab479
AlMa <alina201297@mail.ru>
2016-02-18 18:37:40 +0300

    Исправлен баг скачивания всех аудиозаписей.

--- VVKMusic/Downloader/Downloader.cs
+++ VVKMusic/Downloader/Downloader.cs
@@ -5,45 +5,53 @@ using System.IO;
 using System.ComponentModel;
 using System.Net;
 using System.Windows;
+using System.Threading.Tasks;
 using Status = Common.Common.Status;
 
 namespace Downloader
     {
         public class Downloader : IDownloader
         {
-            int count = 0;
-            public Status DownloadSong(List<Song> songList)
+        int count = 0;
+        public Status DownloadSong(List<Song> songList)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + @"\";
+            count = songList.Count;
+            foreach (Song song in songList)
             {
-                string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + @"\";
-                count = songList.Count;
-                try
-                {
-                    WebClient Client = new WebClient();
-                    foreach (Song song in songList)
-                    {
-                        Client.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCallback);
-                        string fileName = song.Artist + "-" + song.Title + ".mp3";
-                        Client.DownloadFileAsync(song.Uri, @folder + fileName);
-                        song.DownloadedUri = new Uri(@folder + fileName);
-                        song.Downloaded = true;
-                    }
-                }
-                catch (WebException)
+                string fileName = song.Artist + "-" + song.Title + ".mp3";
+                if (DownloadAudioAync(song.Uri, @folder + fileName).Exception != null)
                 {
                     return Status.Error;
                 }
-                catch (ArgumentNullException)
-                {
-                    return Status.Error;
-                }
-                catch (NotSupportedException) { }
-                return Status.OK;
+                song.DownloadedUri = new Uri(@folder + fileName);
+                song.Downloaded = true;
             }
-            public void DownloadFileCallback(object sender, AsyncCompletedEventArgs e)
+            return Status.OK;
+        }
+
+
+        public void DownloadFileCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            count--;
+            if (count == 0)
             {
-                count--;
-                MessageBox.Show("Скачивание завершено полностью" + count.ToString(), "", MessageBoxButton.OK);
+                MessageBox.Show("Скачивание завершено", "", MessageBoxButton.OK);
             }
+        }
+
+        public async Task DownloadAudioAync(Uri url, string path)
+        {
+            try
+            {
+                WebClient Client = new WebClient();
+                Client.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCallback);
+                await Client.DownloadFileTaskAsync(url, path);
+            }
+            catch
+            { }
 
         }
+
     }
+}
